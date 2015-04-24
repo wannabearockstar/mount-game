b3e0662a2ad534d3a5ce68917393fb0e9568bc81
wannabe <smelov.kirill@gmail.com>
2015-04-24 21:01:14 +1000

    add plcaholder for youch down event

--- core/src/com/mygdx/mount/game/manager/GameManager.java
+++ core/src/com/mygdx/mount/game/manager/GameManager.java
@@ -1,5 +1,7 @@
 package com.mygdx.mount.game.manager;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -9,12 +11,13 @@ import com.mygdx.mount.game.manager.game.services.DrawService;
 /**
  * Created by wannabe on 24.04.15.
  */
-public class GameManager extends Stage {
+public class GameManager extends Stage implements InputProcessor {
     DrawService drawService;
     Hero hero;
 
     public GameManager(Viewport viewport, Batch batch) {
         super(viewport, batch);
+        Gdx.input.setInputProcessor(this);
         drawService = new DrawService();
         hero = new Hero();
     }
@@ -24,5 +27,10 @@ public class GameManager extends Stage {
         getBatch().begin();
         drawService.drawHero(hero, getBatch());
         getBatch().end();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return true;
     }
 }
