85c401ebe30fcf5e49323de86078a69b5b6d45ab
Dejibqp <dejibqp@mail.ru>
2015-04-25 21:58:51 +1000

    add StartScreen, fix some changes

--- core/src/com/mygdx/mount/MountGame.java
+++ core/src/com/mygdx/mount/MountGame.java
@@ -9,17 +9,19 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.mygdx.mount.game.manager.GameManager;
 import com.mygdx.mount.game.screens.GameScreen;
+import com.mygdx.mount.game.screens.StartScreen;
 
 public class MountGame extends Game {
 
     GameScreen gameScreen;
+    StartScreen startScreen;
     public static final int CAMERA_HEIGHT = 480;
     public static final int CAMERA_WIDTH = 640;
 
     @Override
     public void create() {
-        gameScreen = new GameScreen(this);
-        setScreen(gameScreen);
+        startScreen = new StartScreen(this);
+        setScreen(startScreen);
     }
 
 }
---
+++ core/src/com/mygdx/mount/game/screens/StartScreen.java
@@ -0,0 +1,106 @@
+package com.mygdx.mount.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.mount.MountGame;
+
+/**
+ * Created by dejibqp on 25.04.15.
+ */
+public class StartScreen implements Screen, InputProcessor {
+    SpriteBatch batch;
+    private static final String TEXTURE_URL = "sprites/start_screen.png";
+    MountGame game;
+    private Texture texture;
+
+    public StartScreen(MountGame game) {
+        batch = new SpriteBatch();
+        this.game = game;
+        texture = new Texture(TEXTURE_URL);
+        Gdx.input.setInputProcessor(this);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(texture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        game.setScreen(new GameScreen(game));
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
