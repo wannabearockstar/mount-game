6081a8dbfd388a2ee471e5cfa1e8223fc3c7e31c
Dejibqp <dejibqp@mail.ru>
2015-04-25 11:49:18 +1000

    Fix GameScreen

--- core/src/com/mygdx/mount/game/screens/GameScreen.java
+++ core/src/com/mygdx/mount/game/screens/GameScreen.java
@@ -3,7 +3,6 @@ package com.mygdx.mount.game.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.mygdx.mount.MountGame;
