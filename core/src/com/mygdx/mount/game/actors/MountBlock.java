c435eb126d48950493ec106bd891ec3ec024f23b
wannabe <smelov.kirill@gmail.com>
2015-04-26 13:08:33 +1000

    merge new level to master. basic pause feature

--- core/src/com/mygdx/mount/game/actors/MountBlock.java
+++ core/src/com/mygdx/mount/game/actors/MountBlock.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 /**
  * Created by dejibqp on 26.04.15.
  */
-public class MountBlock extends BaseBlock{
+public class MountBlock extends BaseBlock {
     public final static String TEXTURE_URL = "sprites/mountain-ground-block.jpg";
     public final static int WIDTH = 30;
     public final static int HEIGHT = 30;
